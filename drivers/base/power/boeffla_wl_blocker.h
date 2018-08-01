b9d66a9f4d20ac12595b646e4b15258df3c3b092
Anthony Asher <acasher754@mail.cfcc.edu>
2018-08-01 12:00:21 -0400

    Disable FLP for now

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -15,8 +15,8 @@
  */
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
-#define LIST_WL_DEFAULT			"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;DIAG_WS;qcom_sap_wakelock"
+#define LIST_WL_DEFAULT			"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;DIAG_WS;qcom_sap_wakelock;ipc000000a4_FLP"
 
 #define LENGTH_LIST_WL			255
-#define LENGTH_LIST_WL_DEFAULT		149
+#define LENGTH_LIST_WL_DEFAULT		165
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
